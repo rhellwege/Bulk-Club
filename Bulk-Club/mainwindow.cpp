8560680e9fdb58551937ae8e7a31825f6e2e89ec
Ryan Hellwege <rhellwege6@gmail.com>
2023-05-09 17:40:30 -0700

    added main tabs, and basic permissions handling. We should refactor to abstract away each tab into its own class

--- Bulk-Club/mainwindow.cpp
+++ Bulk-Club/mainwindow.cpp
@@ -2,12 +2,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QStatusBar>
+#include <QMessageBox>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    permission = Permission::NONE;
+
+    updateLoggedInStatus("Logged in as: N/A");
 }
 
 MainWindow::~MainWindow()
@@ -15,4 +22,50 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::updateLoggedInStatus(const QString &loggedInMessage)
+{
+    ui->statusbar->showMessage(loggedInMessage);
+}
+
+
+
+void MainWindow::on_pushButtonLogin_clicked()
+{
+    QString loginMessage = "Logged in as: ";
+    QString permissionString = ui->comboBoxLogin->currentText();
+
+    updateLoggedInStatus(loginMessage.append(permissionString));
+    // update permission:
+    if (permissionString == "Manager")
+    {
+        permission = Permission::MANAGER;
+    }
+    else if (permissionString == "Administrator")
+    {
+        permission = Permission::ADMINISTRATOR;
+    }
+
+}
+
+
+void MainWindow::on_pushButtonLogOut_clicked()
+{
+    updateLoggedInStatus("Logged in as: N/A");
+    permission = Permission::NONE;
+}
+
+
+void MainWindow::on_tabWidgetMain_currentChanged(int index)
+{
+    if (index == 0) return; // don't bother checking anything if the user chooses the login page
+    switch (permission)
+    {
+    case Permission::NONE:
+        ui->tabWidgetMain->setCurrentIndex(0); // force the tab to switch to the login page
+        // notify the user to login
+        QMessageBox::information(this, "Permissions Error",
+                                 "You cannot accecss this page unless you are logged in as a manager or administrator.");
+        break;
+    }
+}
 
--- Bulk-Club/mainwindow.h
+++ Bulk-Club/mainwindow.h
@@ -4,7 +4,11 @@
 
 #include <QMainWindow>
 
-
+enum Permission {
+    NONE = 0,
+    MANAGER,
+    ADMINISTRATOR
+};
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -19,8 +23,18 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButtonLogin_clicked();
+
+    void on_pushButtonLogOut_clicked();
+
+    void on_tabWidgetMain_currentChanged(int index);
+
 private:
+    // TODO: add permissions enum
+    Permission permission;
     Ui::MainWindow *ui;
+    void updateLoggedInStatus(const QString& loggedInMessage);
 };
 
 #endif // MAINWINDOW_H
